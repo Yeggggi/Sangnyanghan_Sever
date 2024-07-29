27b4addf9bf97aac4fa498c6978208669281836e
Yeggggi <seoyeji998@gmail.com>
2024-07-29 09:33:16 +0900

    firstinit

---
+++ src/main/java/com/example/hellohamsterdemo/HellohamsterdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellohamsterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellohamsterdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohamsterdemoApplication.class, args);
+	}
+
+}
